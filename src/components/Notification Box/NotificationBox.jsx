eab7def984a7da38da216fde50bce2d89ca38574
Mahmoud Elmaghraby <mahmoud.elmaghraby11@gmil.com>
2023-08-15 02:32:18 +0300

    fixing notification grid gap

--- src/components/Notification Box/NotificationBox.jsx
+++ src/components/Notification Box/NotificationBox.jsx
@@ -2,11 +2,11 @@ import PropTypes from 'prop-types';
 
 const NotificationBox = props =>{
     return (
-        <div onClick={props.clicked} className={"not-box cursor-pointer grid sm:grid-cols-10 grid-cols-12 gap-3 items-center rounded-lg px-3 py-2 my-3 mx-4 " + (props.isUnread? "bg-fm-lightGrayishBlue1 shadow-md" : null)}>
-            <div className="not-userImg self-start sm:col-span-1 col-span-2">
+        <div onClick={props.clicked} className={"not-box cursor-pointer grid grid-cols-12 gap-3 items-center rounded-lg px-3 py-2 my-3 mx-4 " + (props.isUnread? "bg-fm-lightGrayishBlue1 shadow-md" : null)}>
+            <div className="not-userImg self-start md:col-span-1 col-span-2">
                 <img src={props.pImg} className="w-14" alt=""/>
             </div>
-            <div className="not-details col-span-8">
+            <div className={"not-details " + (props.notType === 'picComment' ? "col-span-8" : "md:col-span-11 col-span-10")}>
                 <div className="not-msg">
                     <span className="userName font-bold hover:text-blue-900">
                         {props.pName} 
@@ -30,7 +30,7 @@ const NotificationBox = props =>{
             </div>
             {
                 props.notType === 'picComment' ?
-                    <img src={props.commentedPic} className='inline-block w-12 ms-auto sm:col-span-1 col-span-2' alt="" />
+                    <img src={props.commentedPic} className='inline-block w-12 ms-auto col-span-2' alt="" />
                     : null
             }
      </div>
